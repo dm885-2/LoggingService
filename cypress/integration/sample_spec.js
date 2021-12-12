cf5df269acce824697190d854e96488def0e2048
Oliver Rønberg <oliver.rp@ronbergit.dk>
2021-12-12 21:01:40 +0100

    changes to test

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -7,14 +7,35 @@ describe('Loggin Test', () => {
             })
         })
     });
-    it('should log', () => {
-        cy.request("POST", ip + "/signup", {
-            "username": "123",
-            "password": "123",
-            "passwordRepeat": "123"
-        }).then((res) => {
-            return res;
-        });
-    });
+    // create a cypress test, where a signup request is made to the server. The logging services should be tested and add to database
+    describe('Signup Tests', () => {
+        it("Test POST /signup", () => {
+            cy.request({
+                method: 'POST',
+                url: ip + "/signup",
+                body: {
+                    username: "test",
+                    password: "test",
+                    repeatPassword: "test"
+                }
+            }).then((response) => {
+                cy.wrap(response).its('status').should('eq', 200);
+                // Get all logs from database and check if the log from the signup request is in the database
+                cy.request({
+                    method: 'GET',
+                    url: ip + "/logs"
+                }).then((response) => {
+                    cy.wrap(response).its('status').should('eq', 200);
+                    cy.wrap(response.body).its('length').should('eq', 1);
+                    //cy.wrap(response.body[0]).its('username').should('eq', "test");
+                    //cy.wrap(response.body[0]).its('password').should('eq', "test");
+                    //cy.wrap(response.body[0]).its('type').should('eq', "signup");
+                }
 
+                )
+
+            })
+        })
+    });
+    
 });
