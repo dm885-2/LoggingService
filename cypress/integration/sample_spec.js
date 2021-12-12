521654d1718986aedda7b7f716b30ccd12b196e9
Oliver Rønberg <oliver.rp@ronbergit.dk>
2021-12-12 16:17:04 +0100

    add ip to test

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,6 +1,14 @@
+const ip = "http://127.0.0.1:3000"; // Should be url to UI
 describe('Loggin Test', () => {
+    describe('Rapid River Tests', () => {
+        it("Test GET /", () => {  
+            cy.request('GET', ip + "/").then((response) => {
+                cy.wrap(response).its('status').should('eq', 200);
+            })
+        })
+    });
     it('should log', () => {
-        cy.request("POST", "/signup", {
+        cy.request("POST", ip + "/signup", {
             "username": "123",
             "password": "123",
             "passwordRepeat": "123"
