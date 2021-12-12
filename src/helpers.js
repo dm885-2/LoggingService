bcfaafc2687f3d154cccd58f8f140f98bebef801
Oliver Rønberg <oliver.rp@ronbergit.dk>
2021-12-12 21:01:04 +0100

    updates and add getLogs

--- src/helpers.js
+++ src/helpers.js
@@ -2,45 +2,50 @@ import rapid from "@ovcina/rapidriver";
 import jwt from "jsonwebtoken";
 import mysql from "mysql";
 
-
 const SECRET = process.env.SECRET ?? `3(?<,t2mZxj$5JT47naQFTXwqNWP#W>'*Kr!X!(_M3N.u8v}%N/JYGHC.Zwq.!v-`;  // JWT secret
 const rabbitUser = process.env.rabbitUser ?? "guest";
 const rabbitPass = process.env.rabbitPass ?? "guest";
 export const host = "amqp://" + rabbitUser + ":" + rabbitPass + "@" + (process.env.rabbitHost ?? `localhost`);  // RabbitMQ url
 
+
+export function publishAndWait(event, responseEvent, sessionID, data, userID)
+{
+    return new Promise(r => RapidManager.publishAndSubscribe(event, responseEvent, sessionID, data, r, userID));
+}
+
 /**
  * Automatically adds logging, request and sessionIDs to rabbit responses.
- * @param stromg host
- * @param [] subscribers
+ * @param stromg host 
+ * @param [] subscribers 
  */
-export function subscriber(host, subscribers)
-{
-    rapid.subscribe(host, subscribers.map(subscriber => ({
-        river: subscriber.river,
-        event: subscriber.event,
-        work: (msg, publish) => {
-            const wrappedPublish = (event, data) => {
-               let logPath = msg.logPath ?? [];
-               logPath.push({
-                   river: subscriber.river, 
-                   event: subscriber.event
-               });
+ export function subscriber(host, subscribers)
+ {
+     rapid.subscribe(host, subscribers.map(subscriber => ({
+         river: subscriber.river,
+         event: subscriber.event,
+         work: (msg, publish) => {
+             const wrappedPublish = (event, data) => {
+                let logPath = msg.logPath ?? [];
+                logPath.push({
+                    river: subscriber.river, 
+                    event: subscriber.event
+                });
 
-               publish(event, {
-                   ...data,
-                   sessionId: msg.sessionId,
-                   requestId: msg.requestId,
-                   logPath
-               });
-            };
-            subscriber.work(msg, wrappedPublish);
-        },
-    })));
+                publish(event, {
+                    ...data,
+                    sessionId: msg.sessionId,
+                    requestId: msg.requestId,
+                    logPath
+                });
+             };
+             subscriber.work(msg, wrappedPublish);
+         },
+     })));
 }
 
 /**
  * Returns the token payload if its valid, otherwise it returns false.
- * @param String token
+ * @param String token 
  * @returns Promise<false|TokenData>
  */
 export function getTokenData(token)
@@ -58,14 +63,22 @@ if(process.env.mysqlDb)
         database : process.env.mysqlDb ?? 'db',
     });
     connection.connect();
-    query("CREATE TABLE IF NOT EXISTS `logs` (`id` int(11) unsigned NOT NULL AUTO_INCREMENT,`userId` int(11) NOT NULL, `sessionId` varchar(255) NOT NULL,`requestId` varchar(255) NOT NULL,`logPath` TEXT NOT NULL, KEY `Index 1` (`id`)) ENGINE=InnoDB DEFAULT CHARSET=latin1;");
-
+    
+    
+    query("CREATE TABLE IF NOT EXISTS `logs` (" +
+    "`id` int(10) unsigned NOT NULL AUTO_INCREMENT," +
+    "`userId` int(11) NOT NULL," +
+    "`sessionId` varchar(255) NOT NULL," +
+    "`requestId` varchar(255) NOT NULL, " + 
+    "`logPath` varchar(255) NOT NULL, " +
+    "KEY `Index 1` (`id`)" +
+    ") ENGINE=InnoDB DEFAULT CHARSET=latin1;");    
 }
 
 /**
- * Runs a SQL query on the DB.
- * @param string stmt
- * @param ?string[] WHERE
+ * Runs a SQL query on the DB. 
+ * @param string stmt 
+ * @param ?string[] WHERE 
  * @returns results[]|false
  */
 export function query(stmt, WHERE = [])
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import rapid from "@ovcina/rapidriver";
-import {host, query} from "./helpers.js";
+import {host, query, subscriber} from "./helpers.js";
 
 export function format(log){
   let str = "";
@@ -23,14 +23,20 @@ export function format(log){
 
 export async function savelog(msg){
   
-    const newUserStmt = await query("INSERT INTO logs (`userId`,`sessionId`, `requestId`, `logPath`) VALUES (?, ?, ?, ?)", [
-      msg.userId,
-      msg.sessionId,
-      msg.requestId,
-      format(msg.logPath)
-  ]);
+  const log = await query("INSERT INTO `logs` (`userId`, `sessionId`, `requestId`, `logPath`) VALUES (?, ?, ?, ?)", [msg.userId, msg.sessionId, msg.requestId, format(msg.logPath)]);
+  
+}
  
    
+export async function getLogs(msg, publish){
+  console.log("getLogs");
+  const data = await query("SELECT * FROM `logs`");
+  console.log(data);
+  
+  publish("getLogs-response", {
+    data: data || [],
+});
+console.log(data);
 }
 
 if(process.env.RAPID)
@@ -42,4 +48,9 @@ if(process.env.RAPID)
           savelog(msg);
         }
       }]);
+
+     // Create a rapidriver instance that listen for logRequests and return all logs
+     subscriber(host, [{river: "logging", event: "getLogs", work: getLogs}]);
+
+      
 }
