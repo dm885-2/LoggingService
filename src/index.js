f0c84613ef43c6361ef128f5c5a7e0effda4d895
Oliver Rønberg <oliver.rp@ronbergit.dk>
2021-12-12 11:18:48 +0100

    add test to ensure format is working correct

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import rapid from "@ovcina/rapidriver";
 import {host, query} from "./helpers.js";
 
-function format(log){
+export function format(log){
   let str = "";
+  // format should thoughn an eroror if the log is not an array or empty
+  if(!Array.isArray(log) || log.length === 0){
+    throw 500;
+  }
+  
   // Loop though array, split input into right format
   log.forEach(item => {
+    // ensure that the item contains a river and a event, otherwise throw error
+    if(!item.river || !item.event){
+      throw 500;
+    }
+    
     str += `r:${item.river}-e:${item.event}/`
   })
 
--- src/index.test.js
+++ src/index.test.js
@@ -1,8 +1,32 @@
-import {ping} from "./index.js";
+import {format} from "./index.js";
 
-test('ping responds with pong', () => {
-    ping({token: "",}, (channel, message) => {
-        expect(channel).toBe("pong");
-        expect(message.check).toBe(false);
-    });
+test('format of [{"river":"auth","event":"signUp"}] to "r:auth-e:signUp', () => {
+    expect(format([{"river":"auth","event":"signUp"}])).toBe("r:auth-e:signUp");
+  });
+
+test('format of [{"river":"auth","event":"signUp"},{"river":"auth","event":"signUp"}] to "r:auth-e:signUp', () => {
+    expect(format([{"river":"auth","event":"signUp"},{"river":"auth","event":"signUp"}])).toBe("r:auth-e:signUp/r:auth-e:signUp");
+  });
+
+test('format should expect two arguments', () => {
+    expect(() => format()).toThrow();
+  });
+test('format should expect an array as first argument', () => {
+    expect(() => format("string")).toThrow();
+  });
+
+test('format should thow an error if the array is empty', () => {
+    expect(() => format([])).toThrow();
 });
+
+test('format should thow an error if one of the objects is missing a river', () => {
+    expect(() => format([{"event":"signUp"}])).toThrow();
+});
+test('format should thow an error if one of the objects is missing a river', () => {
+    expect(() => format([{"river":"auth"}])).toThrow();
+});
+
+test('format should thow an error if one of the objects is missing a river', () => {
+    expect(() => format([{"river":"auth","event":"signUp"},{"river":"auth"}])).toThrow();
+});
+  
