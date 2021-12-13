a4553fcc5b591097ab461a5f3cff5fe3917b0966
Oliver Rønberg <oliver.rp@ronbergit.dk>
2021-12-13 19:13:16 +0100

    Changes to cypress

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,41 +1,40 @@
-const ip = "http://127.0.0.1:3000"; // Should be url to UI
-describe('Loggin Test', () => {
-    describe('Rapid River Tests', () => {
-        it("Test GET /", () => {  
-            cy.request('GET', ip + "/").then((response) => {
-                cy.wrap(response).its('status').should('eq', 200);
-            })
-        })
-    });
-    // create a cypress test, where a signup request is made to the server. The logging services should be tested and add to database
-    describe('Signup Tests', () => {
-        it("Test POST /signup", () => {
-            cy.request({
-                method: 'POST',
-                url: ip + "/signup",
-                body: {
-                    username: "test",
-                    password: "test",
-                    repeatPassword: "test"
-                }
-            }).then((response) => {
-                cy.wrap(response).its('status').should('eq', 200);
-                // Get all logs from database and check if the log from the signup request is in the database
-                cy.request({
-                    method: 'GET',
-                    url: ip + "/logs"
-                }).then((response) => {
-                    cy.wrap(response).its('status').should('eq', 200);
-                    cy.wrap(response.body).its('length').should('eq', 1);
-                    //cy.wrap(response.body[0]).its('username').should('eq', "test");
-                    //cy.wrap(response.body[0]).its('password').should('eq', "test");
-                    //cy.wrap(response.body[0]).its('type').should('eq', "signup");
-                }
 
-                )
 
-            })
-        })
-    });
+/** 	Sample taken from CRUD test 		**/
+
+describe('READ Test', () => {
+    //add a files before each test
+    beforeEach(()=> {
+        Cypress.Cookies.defaults({
+            preserve: "sessionId"
+          })
+
+        cy.request("POST", "/files", {
+            "filename": "testFile.mzn",
+            "filetype": "mzn",
+            "data": "This is the file content!"
+        }).then((res) => {
+            return;
+        });
+    })
     
+    //Delete all files after each test
+   afterEach(()=>{
+        cy.request('GET', "/files/all/mzn").then(res => {
+            res.body.results.forEach(file => {
+                cy.request('DELETE', "/files/"+file.fileId);
+            })
+            return;
+        });
+   })
+
+   it("READ TEST", () => {
+        cy.request('GET', "/files/all/mzn").then(file => {
+            cy.request("GET", "/files/"+file.body.results[0].fileId).then((res) => {
+                expect(res).to.have.property("status", 200);
+                expect(res.body).to.have.property("error", false);
+                return;
+            });
+        });
+    });
 });
