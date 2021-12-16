6f0d988f7864eb98569bc13facafc1f8ae914580
Oliver Rønberg <oliver.rp@ronbergit.dk>
2021-12-16 14:09:18 +0100

    update on cypress

--- cypress/integration/adminTest.js
+++ cypress/integration/adminTest.js
@@ -15,35 +15,25 @@ let accessToken;
 describe('ADMIN Test', () => {
     //Same test as in Authentication Services, as we just need to create a user and loging
 
-    it("Create Admin login", () => 
-    cy.request('POST', `/auth/register`, {
-        "username": admin.username,
-        "password": admin.password,
-        "passwordRepeat": admin.password,
-        "rank": 1
-    }).then((response1) => {
-        expect(response1.body).to.have.property('error', false)
-    cy.request('POST', `/auth/login`, admin).then((response2) => {
-        expect(response2.body).to.have.property('error', false);
-        refreshToken = response2.body.refreshToken;
+    before(()=> {
+        const uname = "u"+Date.now();
+        const pass = "p"+Date.now();
+        cy.register(uname, pass, 1);
+        cy.login(uname, pass);
+        cy.getAT();
        
-    cy.request('POST', `/auth/accessToken`, {
-        refreshToken
-    }).then((response3) => {
-        expect(response3.body).to.have.property('error', false)
-        accessToken = response3.body.accessToken;
     })
-    });
-    }));
 
     it("Admin will get logs", () => {
         
         cy.request({
             method: "get",
              url: "/logs",
-            headers: {
-                "Authorization": `Bearer ${accessToken}`
-          }}).then(res => {
+             headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer " + Cypress.env("token")
+            }
+        }).then(res => {
             expect(res).to.have.property("status", 200);
 
             return;
--- cypress/integration/userTest.js
+++ cypress/integration/userTest.js
@@ -1,41 +1,22 @@
 // This test will try to access the log as a regular user
 // and will check if the log is accessible.
 // It should be rejectede as the user does not have the right to access the log - 403.
-const time = Date.now();
-const user = {
-    username: `u${time}`,
-    password: `p${time}`,
-};
 
 
-let refreshToken;
-let accessToken;
-
+efore(()=> {
+    const uname = "u"+Date.now();
+    const pass = "p"+Date.now();
+    cy.register(uname, pass, 0);
+    cy.login(uname, pass);
+    cy.getAT();
+    
+})
 
 
 
 describe('USER Test', () => {
 
-    it("Create user login", () => 
-    cy.request('POST', `/auth/register`, {
-        "username": user.username,
-        "password": user.password,
-        "passwordRepeat": user.password
-        
-    }).then((response1) => {
-        expect(response1.body).to.have.property('error', false)
-    cy.request('POST', `/auth/login`, user).then((response2) => {
-        expect(response2.body).to.have.property('error', false);
-        refreshToken = response2.body.refreshToken;
-       
-    cy.request('POST', `/auth/accessToken`, {
-        refreshToken
-    }).then((response3) => {
-        expect(response3.body).to.have.property('error', false)
-        accessToken = response3.body.accessToken;
-    })
-    });
-    }));
+   
 
     it("User will not logs", () => {
         
@@ -43,9 +24,11 @@ describe('USER Test', () => {
             method: "get",
              url: "/logs",
              failOnStatusCode: false,
-            headers: {
-                "Authorization": `Bearer ${accessToken}`
-          }}).then(res => {
+             headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer " + Cypress.env("token")
+            }
+        }).then(res => {
             expect(res).to.have.property("status", 403);
 
             return;
