cc222fbd66212461973b7e481efb78619712e73c
Oliver Rønberg <oliver.rp@ronbergit.dk>
2021-12-17 21:12:17 +0100

    update k3s

--- .github/k3s-setup.sh
+++ .github/k3s-setup.sh
@@ -10,11 +10,12 @@ kubectl create ns rabbits
 # MYSQL
 kubectl apply -f https://raw.githubusercontent.com/DM885/MySQLK8S/main/mysql-secrets.yaml
 kubectl -n rabbits apply -f https://raw.githubusercontent.com/DM885/MySQLK8S/main/mysql-secrets.yaml
+kubectl -n rabbits apply -f https://raw.githubusercontent.com/DM885/MySQLK8S/main/auth-secrets.yaml
 kubectl apply -f https://raw.githubusercontent.com/DM885/MySQLK8S/main/mysql-pv.yaml
 kubectl apply -f https://raw.githubusercontent.com/DM885/MySQLK8S/main/mysql-pvc.yaml
 helm install mysql -f https://raw.githubusercontent.com/DM885/MySQLK8S/main/mysql-values.yaml bitnami/mysql
 
-# # RABBITMQ 
+# # RABBITMQ
 kubectl -n rabbits apply -f https://raw.githubusercontent.com/DM885/RabbitMQK8S/main/rabbit-rbac.yaml
 kubectl -n rabbits apply -f https://raw.githubusercontent.com/DM885/RabbitMQK8S/main/rabbit-configmap.yaml
 kubectl -n rabbits apply -f https://raw.githubusercontent.com/DM885/RabbitMQK8S/main/rabbit-secret.yaml
